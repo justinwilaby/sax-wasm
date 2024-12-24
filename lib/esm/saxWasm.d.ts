87a42ce3562042541aef9c2671f6f7e05558c652
justinwilaby <jwilaby@gmail.com>
2024-12-24 15:55:50 -0800

    fixes #79 - Improve API ergonomics by add parse() method to SAXParser

--- lib/esm/saxWasm.d.ts
+++ lib/esm/saxWasm.d.ts
@@ -97,6 +97,7 @@ export declare class SAXParser {
     private readonly options;
     private writeBuffer?;
     constructor(events?: number, options?: SaxParserOptions);
+    parse(reader: ReadableStreamDefaultReader<Uint8Array>): AsyncGenerator<[SaxEventType, Detail]>;
     write(chunk: Uint8Array): void;
     end(): void;
     prepareWasm(source: Response | Promise<Response>): Promise<boolean>;
